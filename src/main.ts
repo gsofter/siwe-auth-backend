29183e1b18fcd1bfac5110232b4696e06990c7e9
gsofter <real.kingstar@outlook.com>
2023-04-30 20:21:13 -0500

    update allow origin configuration

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', '*'],
+    origin: allowedList,
   });
 
   await app.listen(process.env.PORT || 3002);
